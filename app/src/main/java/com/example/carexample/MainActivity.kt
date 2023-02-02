e69aca14d184764c55eee7ea1a160d214d35c73a
Anas Ahmad <anasfarhad304@gmail.com>
2023-02-03 03:30:28 +0500

    bug fixed commit

--- app/src/main/java/com/example/carexample/MainActivity.kt
+++ app/src/main/java/com/example/carexample/MainActivity.kt
@@ -10,9 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.carexample.ui.theme.CarExampleTheme
 
 class MainActivity : ComponentActivity() {
+    lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,22 +26,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    navController = rememberNavController()
+                    Navigation(navController = navController)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CarExampleTheme {
-        Greeting("Android")
-    }
-}
